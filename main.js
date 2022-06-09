16d13e82fb339be468b13b0867f33b1e1b4b30c8
dnwn-9001 <j10511@hanmail.net>
2022-06-10 01:46:41 +0900

    JS : Navbar Menu Click

--- main.js
+++ main.js
@@ -13,3 +13,18 @@ document.addEventListener("scroll", () => {
     navbar.classList.remove("navbar--dark");
   }
 });
+
+// Navbar 클릭시 원하는 위치로 스크롤링
+// Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector(".navbar__menu");
+navbarMenu.addEventListener("click", (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+
+  if (link == null) {
+    return;
+  }
+
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({ behavior: "smooth" });
+});
